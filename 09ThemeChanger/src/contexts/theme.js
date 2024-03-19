88bc5b5622ba4201f1a3662fa576f53255655720
Anish Gautam <anishgautam2024@gmail.com>
2024-03-19 17:13:24 +0545

    Theme toogler using react useContext ready

---
+++ 09ThemeChanger/src/contexts/theme.js
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: ()=>{},
+    lightTheme: ()=>{},
+
+})
+
+export const ThemeProvider= ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
