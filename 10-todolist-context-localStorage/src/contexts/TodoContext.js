2f5d08f4869216e74f308dea5b9115f056a7b853
Anish Gautam <anishgautam2024@gmail.com>
2024-03-20 19:16:21 +0545

    Project Ready

--- 10-todolist-context-localStorage/src/contexts/TodoContext.js
+++ 10-todolist-context-localStorage/src/contexts/TodoContext.js
@@ -2,26 +2,11 @@ import { useContext, createContext } from "react";
 
 export const TodoContext = createContext({
     todos: [
-        {
-            id:1,
-            todo: "Todo message",
-            completed: false,
-        },
-        {
-            id:2,
-            todo: "Todo message",
-            completed: false,
-        },
-        {
-            id:3,
-            todo: "Todo message",
-            completed: false,
-        },
-        {
-            id:4,
-            todo: "Todo message",
-            completed: false,
-        }
+       {
+        id:1,
+        todo: "A message",
+        completed: false
+       }
     ],
     addTodo: (todo)=>{},
     updateTodo: (id, todo) =>{},
