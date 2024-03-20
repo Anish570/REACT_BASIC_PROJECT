8ba6b3cdce3e9c86c4544279c9b67c927312eb47
Anish Gautam <anishgautam2024@gmail.com>
2024-03-20 10:21:42 +0545

    context component's funcationalites ready

--- 10-todolist-context-localStorage/src/App.jsx
+++ 10-todolist-context-localStorage/src/App.jsx
@@ -14,6 +14,11 @@ function App() {
    const deleteTodo = (id)=>{
       setTodos((prev)=> prev.filter((todo)=> todo.id !== id))
    }
+   const toggleComplete = (id)=>{
+    setTodos((prev)=> prev.map((prevtodo)=>(
+      prevtodo.id === id ? {...prevtodo, completed: !prevtodo.completed} : prevtodo
+    )))
+   }
   return (
   <TodoProvider value={{ todos,addTodo,deleteTodo,updateTodo,toggleComplete}}>
    <div className="bg-[#172842] min-h-screen py-8">
