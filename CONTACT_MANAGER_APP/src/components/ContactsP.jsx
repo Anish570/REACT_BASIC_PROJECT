45a164458c7e8a4df878157741905e41405cdf1b
Anish Gautam <anishgautam2024@gmail.com>
2024-04-13 12:58:58 +0545

    added delete functionality

--- CONTACT_MANAGER_APP/src/components/ContactsP.jsx
+++ CONTACT_MANAGER_APP/src/components/ContactsP.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import { HiOutlineUserCircle } from "react-icons/hi2";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import { deleteDoc, doc } from 'firebase/firestore';
+import { db } from '../config/firebase';
 
 
 const ContactsP = ({data}) => {
-    console.log("props received: ",data)
+   const deleteContact = async(id) =>{
+    try {
+      await deleteDoc(doc(db,"contact",id))
+    } catch (error) {
+      console.log(error)
+    }
+   }
   return (
     <div className='flex gap-2 p-2 items-center bg-yellow rounded-lg text-black mb-[15px] '>
         <HiOutlineUserCircle className='text-4xl text-orange'/>
@@ -15,7 +23,7 @@ const ContactsP = ({data}) => {
         </div>
         <div className='text-2xl flex items-center'>
             <FaEdit/>
-            <MdDelete/>
+            <MdDelete onClick={()=>{deleteContact(data.id)}} className='text-red-700'/>
         </div>
     </div>
   )
