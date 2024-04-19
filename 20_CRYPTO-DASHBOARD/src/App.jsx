796594c3d55cae859068cccfc3f6568585f5be86
Anish Gautam <anishgautam2024@gmail.com>
2024-04-19 16:15:49 +0545

    project setup

---
+++ 20_CRYPTO-DASHBOARD/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Dashboard from './Components/pages/Dashboard/Dashboard'
+
+function App() {
+
+
+  return (
+    <>
+      <Dashboard />
+
+    </>
+  )
+}
+
+export default App
---
+++ 20_CRYPTO-DASHBOARD/src/Components/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideNav from '../../SideBar/SideNav'
+import { Box } from '@chakra-ui/react'
+
+const Dashboard = () => {
+    return (
+        <Box>
+            <SideNav />
+        </Box>
+    )
+}
+
+export default Dashboard
