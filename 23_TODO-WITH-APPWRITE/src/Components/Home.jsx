6dafd552a88b0e26ee8114ddda009a4d0c40eac0
Anish Gautam <anishgautam2024@gmail.com>
2024-05-01 11:22:41 +0545

    Ui of home page change

--- 23_TODO-WITH-APPWRITE/src/Components/Home.jsx
+++ 23_TODO-WITH-APPWRITE/src/Components/Home.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Home = ({ }) => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen px-[10px] md-px-[0px]  text-center bg-gray-100">
-            <h1 className="text-4xl font-bold text-gray-900 mb-8">Welcome to Our Website</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen px-[10px] md-px-[0px]  text-center bg-gray-800 text-white">
+            <h1 className="text-4xl font-bold mb-8">Welcome to Our Website</h1>
             <div className=''>
-                <p className="text-lg text-gray-700 mb-8">Please sign up or log in to access the full features of our website.</p>
+                <p className="text-lg  mb-8">Please sign up or log in to access the full features of our website.</p>
                 <div className="flex space-x-4 justify-center">
                     <Link to="/login" className="text-indigo-600 font-medium hover:underline">Log in</Link>
-                    <span className="text-gray-600">or</span>
+                    <span >or</span>
                     <Link to="/register" className="text-indigo-600 font-medium hover:underline">Sign up</Link>
                 </div>
             </div>
