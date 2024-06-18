5a1993809fb00ac2c32ae88075a4c002e4043c52
anish <anishgautam2024@gmail.com>
2024-06-19 00:11:51 +0545

    practise complete

--- SKELETON_LOADING/src/App.jsx
+++ SKELETON_LOADING/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import { useState } from 'react'
 import Card from './components/Card'
 import Skeleton from './components/Skeleton'
+import { useEffect } from 'react';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
   return (
     <div className='flex items-center justify-center h-screen w-full'>
-      <Skeleton />
-      <Card />
+      {
+        loading ? (<Skeleton />) : (<Card />)
+      }
+
     </div>
   )
 }
