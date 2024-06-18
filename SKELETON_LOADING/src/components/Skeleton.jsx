e186f3ec5470e3d30e7be1262509ff94bdb90f56
anish <anishgautam2024@gmail.com>
2024-06-19 00:05:54 +0545

    skeleton card ready

--- SKELETON_LOADING/src/components/Skeleton.jsx
+++ SKELETON_LOADING/src/components/Skeleton.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
 
 const Skeleton = () => {
     return (
-        <div className='border p-4 max-w-[500px] w-[80%] h-[80%] rounded-md shadow-2xl animate-pulse'>
+        <div className='border p-4 max-w-[500px] w-[80%] h-[80%] rounded-md shadow-2xl '>
             <div className='mb-4 py-2 px-8 h-[60%]'>
-                <div className="w-full h-full object-contain bg-gray-400 animate-pulse" />
+                <div className="w-full h-full object-contain bg-gray-400 rounded-lg animate-pulse" />
             </div>
-            <div>
-                <div className="font-semibold bg-slate-400 w-60 h-4 "> </div><br />
-                <div className="font-semibold text-sm bg-slate-400 w-52  h-4"></div><br />
-                <p className="font-thin w-full h-20 bg-slate-400">
-
+            <div className='space-y-3'>
+                <div className="bg-slate-400 w-[40%] h-4 rounded-lg animate-pulse "> </div>
+                <div className="text-sm bg-slate-400 rounded-lg w-[20%] h-4 animate-pulse"></div>
+                <p className="w-full h-20 bg-slate-400 rounded-lg animate-pulse">
                 </p>
-
+                <p className='w-[30%] rounded-lg float-start bg-slate-400 h-4'></p>
             </div>
         </div>
     )
