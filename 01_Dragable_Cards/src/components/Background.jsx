655e16356b0681334f744b8659e3d17c8109052b
anish <anishgautam2024@gmail.com>
2024-07-16 14:53:10 +0545

    project image added

--- 01_Dragable_Cards/src/components/Background.jsx
+++ 01_Dragable_Cards/src/components/Background.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Background() {
   return (
-   <>
-    <div className='fixed z-[2] w-screen h-screen'>
-    <div className='flex justify-center py-10 text-zinc-500 text-xl font-semibold'>Nav-bar</div>
-     <h1 className='text-zinc-500 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] font-semibold tracking-tighter leading-none'>
-        Docs.
-      </h1>
-    </div>
-      </>
+    <>
+      <div className='fixed z-[2] w-screen h-screen'>
+        <div className='flex justify-center py-10 text-zinc-500 text-xl font-semibold'>Nav-bar</div>
+        <h1 className='text-zinc-500 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] font-semibold tracking-tighter leading-none'>
+          Docs.
+        </h1>
+      </div>
+    </>
   )
 }
 
